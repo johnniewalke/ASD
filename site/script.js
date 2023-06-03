6cbcb6575f0b6757f5b54f8cd3b60f40fc1e942f
Johnnie Menges Giacomelli <johnniegiacomelli@gmail.com>
2023-06-03 20:37:02 -0300

    refacture: ajustado arquivo js com cod com ligação

--- site/script.js
+++ site/script.js
@@ -1,22 +1,10 @@
-// Função para alternar a exibição do menu responsivo
-function toggleMenu() {
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('show');
-  }
-  
-  // Função para rolar a página suavemente até o topo
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  }
-  
-  // Função para validar o formulário de contato
-  function validateForm() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+// script.js
+
+// Validação do formulário de contato
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
   
     if (name === '' || email === '' || message === '') {
       alert('Por favor, preencha todos os campos do formulário.');
